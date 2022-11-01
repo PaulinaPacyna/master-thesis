e0982868b765a7a09473e9be532847b333b453b0
PaulinaPacyna <pacyna1998@gmail.com>
2022-11-01 16:37:54 +0100

    interpolate

--- src/preprocessing/__init__.py
+++ src/preprocessing/__init__.py
@@ -1,10 +1,18 @@
 import scipy.io.arff
 import pandas as pd
+import numpy as np
+from scipy.interpolate import interp1d
 from sklearn.preprocessing import MinMaxScaler
 
 
 def read_ts(path):
     return scipy.io.arff.loadarff(path)
+
+
+def stretch_interpolate(df: pd.Series, target_length: int, type_: str = 'linear') -> pd.Series:
+    interpolator = interp1d(df.index, df.values, kind=type_)
+    new_index = np.linspace(0, len(df) - 1, target_length)
+    return pd.Series(interpolator(new_index))
 
 
 class TargetEncoder:
@@ -24,5 +32,5 @@ class TargetEncoder:
     def get_0_1_scaled(self):
         return self.__scaler.fit_transform(self.categorical)
 
-    def reverse_0_1_scaled(self, X: pd.Series):
+    def reverse_0_1_scale(self, X: pd.Series):
         return self.__scaler.inverse_transform(X)
