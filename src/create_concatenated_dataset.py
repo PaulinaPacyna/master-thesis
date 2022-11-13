099a5983ee7623d8d3edbbae8aef2d31dc075564
PaulinaPacyna <pacyna1998@gmail.com>
2022-11-13 01:47:59 +0100

    shape unification

--- src/create_concatenated_dataset.py
+++ src/create_concatenated_dataset.py
@@ -1,22 +1,20 @@
 import os
 
 import numpy as np
-from sktime.datasets import load_from_tsfile
 
-from src.preprocessing import get_paths, TargetEncoder
+from src.preprocessing import get_paths, TargetEncoder, read_univariate_ts
 
 
-def create_contatenated(root_data_path='data/') -> (np.array, np.array):
-    global X_final, y_final
+def create_concatenated(root_data_path='data/') -> (np.array, np.array):
     X_final = []
     y_final = []
     for path in get_paths(root_data_path):
-        X, y = load_from_tsfile(path)
-        y = TargetEncoder(y).get_categorical_column(prefix=path.split(os.sep)[2])
+        X, y = read_univariate_ts(path)
+        y = TargetEncoder(y).get_categorical_column(prefix=path.split(os.sep)[1])
 
         X_final.append(X)
         y_final.append(y)
-    return np.concatenate(X_final), np.concatenate(y_final)
+    return np.concatenate(X_final, dtype = 'object'), np.concatenate(y_final)
 
 
 def save_datasets(X_final, y_final, SAVING_DATA_PATH='data/concatenated/'):
@@ -27,5 +25,5 @@ def save_datasets(X_final, y_final, SAVING_DATA_PATH='data/concatenated/'):
         np.save(file, y_final)
 
 
-X_final, y_final = create_contatenated()
+X_final, y_final = create_concatenated()
 save_datasets(X_final, y_final)
