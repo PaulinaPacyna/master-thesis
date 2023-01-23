53b389630c80d4cd81f9a3141f4e963860ccbd06
PaulinaPacyna <pacyna1998@gmail.com>
2023-01-23 12:08:17 +0100

    fix __len__ in fit_generator

---
+++ src/mlruns/0/137d216f5e4b4b31992d7ec75fd8905a/artifacts/models/FCN.py
@@ -0,0 +1,27 @@
+import tensorflow.keras as keras
+
+
+def FCN_model(number_of_classes: int, input_length: int = None, parameters = 1) -> keras.models.Model:
+    def inner(input_layer: keras.layers.Input = keras.layers.Input((input_length, 1))):
+        conv1 = keras.layers.Conv1D(
+            filters=int(128*parameters), kernel_size=11, padding="same", input_shape=(None, 1)
+        )(input_layer)
+        conv1 = keras.layers.BatchNormalization()(conv1)
+        conv1 = keras.layers.Activation(activation="relu")(conv1)
+
+        conv2 = keras.layers.Conv1D(filters=int(256*parameters), kernel_size=5, padding="same")(conv1)
+        conv2 = keras.layers.BatchNormalization()(conv2)
+        conv2 = keras.layers.Activation("relu")(conv2)
+
+        conv3 = keras.layers.Conv1D(int(128*parameters), kernel_size=3, padding="same")(conv2)
+        conv3 = keras.layers.BatchNormalization()(conv3)
+        conv3 = keras.layers.Activation("relu")(conv3)
+
+        gap_layer = keras.layers.GlobalAveragePooling1D()(conv3)
+
+        output_layer = keras.layers.Dense(number_of_classes, activation="softmax")(
+            gap_layer
+        )
+        return output_layer
+
+    return inner
