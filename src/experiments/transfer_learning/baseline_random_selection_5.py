5401d7a53df63fa972c1e45fdd544da064d980df
PaulinaPacyna <pacyna1998@gmail.com>
2023-05-07 20:52:51 +0200

    added experiments for random selection

---
+++ src/experiments/transfer_learning/baseline_random_selection_5.py
@@ -0,0 +1,10 @@
+from experiments.transfer_learning.baseline_dba_selection_5 import main
+
+
+main(
+    category="MOTION",
+    selection_method="random",
+    model_type="fcn",
+    single_model_experiment_id="183382388301527558",
+    this_experiment_id="490119298227073255",
+)
---
+++ src/experiments/transfer_learning/ensemble_random_selection_5.py
@@ -0,0 +1,9 @@
+from experiments.transfer_learning.ensemble_dba_selection_5 import main
+
+main(
+    category="MOTION",
+    selection_method="random",
+    this_experiment_id="303093782117013484",
+    component_experiment_id="183382388301527558",
+    no_tr_experiment_id="541913567164685548",
+)
