83f2e7954f5f84de0b4daf77204250e33443d906
Paulina Pacyna <62997314+PaulinaPacyna@users.noreply.github.com>
2023-05-17 09:39:15 +0200

    Results refactor (#8)
    * added approach name
    
    * first and second key names
    
    * fixed typo
    
    * changed logic to dict
    
    * renamed to 1st and second
    
    * renamed fixed
    
    * excluding source
    
    * key name
    
    * generalizws plot kwargs
    
    * baseline vs ensemble
    
    * params vs acc
    
    * dba vs random
    
    * recalculated plots
    
    * added 5 versus 8
    
    * changed font
    
    * fixed labeles
    
    * 3 datasets
    
    * started methodology
    
    * further described baseline and ensemble approach

--- src/results/baseline/get_metrics_per_epoch.py
+++ src/results/baseline/get_metrics_per_epoch.py
@@ -1,44 +1,46 @@
 import os
+from typing import Dict
 
+from mlflow.entities import Run
 from results.utils import Results
 
 
 class BaselineResults(Results):
+    approach_name = "Baseline"
     results_root_path = os.path.dirname(__file__)
-    transfer_learning_key_name_loss = "baseline_loss"
-    transfer_learning_key_name_val_loss = "baseline_val_loss"
-    transfer_learning_key_name_acc = "baseline_accuracy"
-    transfer_learning_key_name_val_acc = "baseline_val_accuracy"
-    no_transfer_learning_key_name_loss = "baseline_no_transfer_learning_base_loss"
-    no_transfer_learning_key_name_val_loss = (
-        "baseline_no_transfer_learning_base_val_loss"
-    )
-    no_transfer_learning_key_name_acc = "baseline_no_transfer_learning_base_accuracy"
-    no_transfer_learning_key_name_val_acc = (
-        "baseline_no_transfer_learning_base_val_accuracy"
-    )
+    first_result_key_name_loss = "baseline_loss"
+    first_result_key_name_val_loss = "baseline_val_loss"
+    first_result_key_name_acc = "baseline_accuracy"
+    first_result_key_name_val_acc = "baseline_val_accuracy"
+    second_result_key_name_loss = "base_loss"
+    second_result_key_name_val_loss = "base_val_loss"
+    second_result_key_name_acc = "base_accuracy"
+    second_result_key_name_val_acc = "base_val_accuracy"
 
-    def _get_transfer_learning_runs(self):
-        hist = self._get_history_per_experiment(self.transfer_learning_experiment_id)
-        return [run for run in hist if run.info.run_name.startswith("Destination")]
+    def _get_first_experiment_runs(self) -> Dict[str, Run]:
+        hist = self._get_history_per_experiment(
+            self.first_experiment_id, exclude_from_name="Source"
+        )
+        return hist
 
     def _prepare_legend(self, text: str):
         mapping = {
-            self.transfer_learning_key_name_loss: "Loss - train split",
-            self.transfer_learning_key_name_val_loss: "Loss - validation split",
-            self.transfer_learning_key_name_acc: "Accuracy - train split",
-            self.transfer_learning_key_name_val_acc: "Accuracy - validation split",
-            self.no_transfer_learning_key_name_loss: "No transfer learning - loss - train split",
-            self.no_transfer_learning_key_name_val_loss: "No transfer learning - loss - validation split",
-            self.no_transfer_learning_key_name_acc: "No transfer learning - accuracy - train split",
-            self.no_transfer_learning_key_name_val_acc: "No transfer learning - accuracy - validation split",
+            self.first_result_key_name_loss: "Loss - train split",
+            self.first_result_key_name_val_loss: "Loss - validation split",
+            self.first_result_key_name_acc: "Accuracy - train split",
+            self.first_result_key_name_val_acc: "Accuracy - validation split",
+            self.second_result_key_name_loss: "No transfer learning - loss - train split",
+            self.second_result_key_name_val_loss: "No transfer learning - loss - validation split",
+            self.second_result_key_name_acc: "No transfer learning - accuracy - train split",
+            self.second_result_key_name_val_acc: "No transfer learning - accuracy - validation split",
         }
         return mapping[text]
 
 
 results = BaselineResults(
-    transfer_learning_experiment_id="743133642334170939",
-    no_transfer_learning_experiment_id="183382388301527558",
+    first_experiment_id="743133642334170939",
+    second_experiment_id="183382388301527558",
+    assert_=False,
 )
 results.get_mean_loss_acc_per_epoch("loss")
 results.get_mean_loss_acc_per_epoch("acc")
--- src/results/utils.py
+++ src/results/utils.py
@@ -3,7 +3,7 @@ import os
 from abc import ABCMeta
 from abc import abstractmethod
 from collections import Counter
-from typing import List
+from typing import Dict
 from typing import Literal
 
 import matplotlib
@@ -14,111 +14,115 @@ from mlflow.entities import Run
 
 
 class Results(metaclass=ABCMeta):
-    results_root_path = os.path.dirname(__file__)
-
     @property
     @abstractmethod
-    def transfer_learning_key_name_loss(self):
+    def results_root_path(self):
         pass
 
     @property
     @abstractmethod
-    def transfer_learning_key_name_val_loss(self):
+    def approach_name(self):
         pass
 
     @property
     @abstractmethod
-    def transfer_learning_key_name_acc(self):
+    def first_result_key_name_loss(self):
         pass
 
     @property
     @abstractmethod
-    def transfer_learning_key_name_val_acc(self):
+    def first_result_key_name_val_loss(self):
         pass
 
     @property
     @abstractmethod
-    def no_transfer_learning_key_name_loss(self):
+    def first_result_key_name_acc(self):
         pass
 
     @property
     @abstractmethod
-    def no_transfer_learning_key_name_val_loss(self):
+    def first_result_key_name_val_acc(self):
         pass
 
     @property
     @abstractmethod
-    def no_transfer_learning_key_name_acc(self):
+    def second_result_key_name_loss(self):
         pass
 
     @property
     @abstractmethod
-    def no_transfer_learning_key_name_val_acc(self):
+    def second_result_key_name_val_loss(self):
+        pass
+
+    @property
+    @abstractmethod
+    def second_result_key_name_acc(self):
+        pass
+
+    @property
+    @abstractmethod
+    def second_result_key_name_val_acc(self):
         pass
 
     def __init__(
         self,
-        transfer_learning_experiment_id: str,
-        no_transfer_learning_experiment_id: str,
+        first_experiment_id: str,
+        second_experiment_id: str,
+        assert_: bool = True,
     ):
-        self.no_transfer_learning_experiment_id = no_transfer_learning_experiment_id
-        self.transfer_learning_experiment_id = transfer_learning_experiment_id
+        self.second_experiment_id = second_experiment_id
+        self.first_experiment_id = first_experiment_id
         self.client = MlflowClient()
-        self.transfer_learning_runs: List[Run] = self._get_transfer_learning_runs()
-        self.no_transfer_learning_runs: List[
-            Run
-        ] = self._get_no_transfer_learning_runs()
-        self._assert_histories_equal()
-        matplotlib.rc("font", size=12)
+        self.first_experiment_runs: Dict[str, Run] = self._get_first_experiment_runs()
+        self.second_experiment_runs: Dict[str, Run] = self._get_second_experiment_runs()
+        if assert_:
+            self._assert_histories_equal()
+        self.datasets = self._get_common_datasets()
+        matplotlib.rc("font", size=9)
 
-    def _get_no_transfer_learning_runs(self):
-        return self._get_history_per_experiment(self.no_transfer_learning_experiment_id)
+    def _get_second_experiment_runs(self) -> Dict[str, Run]:
+        return self._get_history_per_experiment(self.second_experiment_id)
 
-    def _get_transfer_learning_runs(self):
-        return self._get_history_per_experiment(self.transfer_learning_experiment_id)
+    def _get_first_experiment_runs(self) -> Dict[str, Run]:
+        return self._get_history_per_experiment(self.first_experiment_id)
 
-    def _get_history_per_experiment(self, experiment_id):
-        runs = self.client.search_runs([experiment_id])
-        for run in runs:
+    def _get_history_per_experiment(
+        self, experiment_id, add_prefix="", exclude_from_name=None
+    ) -> Dict[str, Run]:
+        result = []
+        for run in self.client.search_runs([experiment_id]):
             if run.info.status == "FINISHED" and run.info.lifecycle_stage == "active":
-                run.data.metrics["history"] = json.load(
-                    open(run.info.artifact_uri + "/history.json")
+                if not exclude_from_name or (
+                    exclude_from_name not in run.info.run_name
+                ):
+                    history = json.load(open(run.info.artifact_uri + "/history.json"))
+                    history = {
+                        add_prefix + key: value for key, value in history.items()
+                    }
+                    run.data.metrics["history"] = history
+                    assert (
+                        min([len(x) for x in run.data.metrics["history"].values()])
+                        == 10
+                    )
+                    result.append(run)
+        datasets_counts = Counter([run.data.params["dataset_train"] for run in result])
+        for dataset_name in datasets_counts:
+            if datasets_counts[dataset_name] > 1:
+                raise ValueError(
+                    f"More than one experiment for {dataset_name} "
+                    f"for {self.first_experiment_id}"
                 )
-                assert min([len(x) for x in run.data.metrics["history"].values()]) == 10
-        return runs
+        return {run.data.params["dataset_train"]: run for run in result}
 
     def _assert_histories_equal(self):
-        transfer_learning_datasets = [
-            run.data.params["dataset_train"] for run in self.transfer_learning_runs
-        ]
-        no_transfer_learning_datasets = [
-            run.data.params["dataset_train"] for run in self.transfer_learning_runs
-        ]
-        transfer_learning_datasets_counts = Counter(transfer_learning_datasets)
-        for dataset_name in transfer_learning_datasets_counts:
-            if transfer_learning_datasets_counts[dataset_name] > 1:
-                raise ValueError(
-                    f"More than one experiment for {dataset_name} "
-                    f"for {self.transfer_learning_experiment_id}"
-                )
-        del transfer_learning_datasets_counts
-        no_transfer_learning_datasets_counts = Counter(no_transfer_learning_datasets)
-        for dataset_name in no_transfer_learning_datasets_counts:
-            if no_transfer_learning_datasets_counts[dataset_name] > 1:
-                raise ValueError(
-                    f"More than one experiment for {dataset_name} "
-                    f"for {self.no_transfer_learning_experiment_id}"
-                )
-
-        del no_transfer_learning_datasets_counts
-        no_transfer_learning_datasets = set(no_transfer_learning_datasets)
-        transfer_learning_datasets = set(transfer_learning_datasets)
-        if no_transfer_learning_datasets != transfer_learning_datasets:
+        second_datasets = set(self.second_experiment_runs.keys())
+        first_datasets = set(self.first_experiment_runs.keys())
+        if second_datasets != first_datasets:
             raise ValueError(
-                f"The following datasets missing for {self.transfer_learning_experiment_id}: "
-                f"{no_transfer_learning_datasets-transfer_learning_datasets}.\n"
-                f"The following datasets missing for {self.no_transfer_learning_experiment_id}: "
-                f"{transfer_learning_datasets-no_transfer_learning_datasets}.\n"
+                f"The following datasets missing for {self.first_experiment_id}: "
+                f"{second_datasets-first_datasets}.\n"
+                f"The following datasets missing for {self.second_experiment_id}: "
+                f"{first_datasets-second_datasets}.\n"
             )
 
     @abstractmethod
@@ -135,32 +139,52 @@ class Results(metaclass=ABCMeta):
         full_metric_name = "accuracy" if metric == "acc" else metric
         figure, ax = plt.subplots(figsize=(5.5, 5.5))
         for metric_name in sorted(history_summarized):
+            plot_kwargs = self._get_plot_kwargs(metric_name)
             plt.plot(
                 np.arange(len(history_summarized[metric_name])) + 1,
                 history_summarized[metric_name],
                 label=metric_name,
-                color="red" if "train" in metric_name else "green",
-                linestyle="--" if "no transfer" in metric_name.lower() else "-",
                 axes=ax,
+                **plot_kwargs,
             )
-        figure.suptitle(f"Model {full_metric_name}")
+        figure.suptitle(f"Model {full_metric_name} - {self.approach_name} approach")
         ax.set_ylabel(full_metric_name)
         ax.set_xlabel("epoch")
+        if metric == "loss":
+            plt.ylim(bottom=0)
+        else:
+            plt.ylim([0, 1])
         ax.legend()
-        plt.ylim(bottom=0)
         plt.savefig(os.path.join(self.results_root_path, f"{metric}.png"))
         plt.close(figure)
 
     def _get_history_summarized_per_epoch(self):
-        metrics_names = self.transfer_learning_runs[0].data.metrics["history"].keys()
+        metrics_names_1 = [
+            self.first_result_key_name_acc,
+            self.first_result_key_name_val_acc,
+            self.first_result_key_name_loss,
+            self.first_result_key_name_val_loss,
+        ]
+        metrics_names_2 = [
+            self.second_result_key_name_acc,
+            self.second_result_key_name_val_acc,
+            self.second_result_key_name_loss,
+            self.second_result_key_name_val_loss,
+        ]
         metrics_per_epoch = {
-            self._prepare_legend(metric): [] for metric in metrics_names
+            self._prepare_legend(metric): []
+            for metric in metrics_names_1 + metrics_names_2
         }
-        for run in self.transfer_learning_runs:
-            history = run.data.metrics["history"]
-            for metric_name in metrics_names:
+        for dataset in self.datasets:
+            history_1 = self.first_experiment_runs[dataset].data.metrics["history"]
+            history_2 = self.second_experiment_runs[dataset].data.metrics["history"]
+            for metric_name in metrics_names_1:
                 metrics_per_epoch[self._prepare_legend(metric_name)].append(
-                    history[metric_name]
+                    history_1[metric_name]
+                )
+            for metric_name in metrics_names_2:
+                metrics_per_epoch[self._prepare_legend(metric_name)].append(
+                    history_2[metric_name]
                 )
         history_summarized = {
             metric: np.array(metrics_per_epoch[metric]).mean(0)
@@ -171,14 +195,14 @@ class Results(metaclass=ABCMeta):
     def win_tie_loss_diagram(self, epoch):
         epoch_acc_pairs = [
             [
-                run.data.metrics["history"][self.no_transfer_learning_key_name_val_acc][
-                    epoch - 1
-                ],
-                run.data.metrics["history"][self.transfer_learning_key_name_val_acc][
-                    epoch - 1
-                ],
+                self.second_experiment_runs[dataset].data.metrics["history"][
+                    self.second_result_key_name_val_acc
+                ][epoch - 1],
+                self.first_experiment_runs[dataset].data.metrics["history"][
+                    self.first_result_key_name_val_acc
+                ][epoch - 1],
             ]
-            for run in self.transfer_learning_runs
+            for dataset in self.datasets
         ]
         win = sum(acc[0] < acc[1] for acc in epoch_acc_pairs)
         tie = sum(acc[0] == acc[1] for acc in epoch_acc_pairs)
@@ -186,7 +210,9 @@ class Results(metaclass=ABCMeta):
         figure, ax = plt.subplots(figsize=(5.5, 5.5))
         plt.scatter(*list(zip(*epoch_acc_pairs)), s=8)
         plt.plot([-10, 10], [-10, 10], color="black")
-        figure.suptitle(f"Win/tie/lose plot (epoch {epoch})")
+        figure.suptitle(
+            f"Win/tie/lose plot - {self.approach_name} approach (epoch {epoch})"
+        )
         ax.set_ylabel("With transfer learning")
         ax.set_xlabel("Without transfer learning")
         plt.xlim([0, 1])
@@ -200,5 +226,38 @@ class Results(metaclass=ABCMeta):
         )
         plt.savefig(
             os.path.join(self.results_root_path, f"win_tie_lose_epoch_{epoch}.png")
+        )
+        plt.close(figure)
+
+    def _get_common_datasets(self) -> list:
+        first_datasets = self.first_experiment_runs.keys()
+        second_datasets = self.second_experiment_runs.keys()
+        return list(set(first_datasets).intersection(second_datasets))
+
+    @staticmethod
+    def _get_plot_kwargs(metric_name) -> dict:
+        return {
+            "color": "red" if "train" in metric_name else "green",
+            "linestyle": "--" if "no transfer" in metric_name.lower() else "-",
+        }
+
+    def dba_vs_accuracy(self) -> dict:
+        results = self.first_experiment_runs
+        sim_acc_pairs = [
+            (
+                results[dataset].data.metrics["Mean DBA similarity"],
+                results[dataset].data.metrics["val_accuracy"],
+            )
+            for dataset in self.datasets
+        ]
+        figure, ax = plt.subplots(figsize=(5.5, 5.5))
+        plt.scatter(*list(zip(*sim_acc_pairs)), s=8)
+        figure.suptitle("Accuracy versus mean DBA similarity of source datasets")
+        ax.set_ylabel("Accuracy - validation split")
+        ax.set_xlabel("Mean DBA similarity of source datasets to target dataset")
+        plt.ylim([0, 1])
+        plt.xscale("log")
+        plt.savefig(
+            os.path.join(self.results_root_path, "accuracy_vs_mean_dba_sim.png")
         )
         plt.close(figure)
